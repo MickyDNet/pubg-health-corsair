fff2110f5c15c4cd8d11b477a7ee55638f31a0da
Tymur Mudzhyri <intrueder@gmail.com>
2019-08-14 14:19:15 +0300

    Migrate to `cue-sdk` package

--- index.js
+++ index.js
@@ -1,45 +1,56 @@
-var CueSDK = require('cue-sdk-node'),
-	robot = require("robotjs"),
-	hexRgb = require('hex-rgb');
+const cue = require('cue-sdk'),
+  robot = require("robotjs"),
+  hexRgb = require('hex-rgb');
 
-var cue = new CueSDK.CueSDK(false, true);
+  //-------------------------------------//
+  //    CONFIG - YOU CAN CHANGE THESE    //
+  //-------------------------------------//
+  var pubghealthx = 1456;
+  var pubghealthy = 1383;
+  //var mouse = robot.getMousePos();
+  //console.log(mouse);
+  
+  //-------------------------------------//
+  //  DONT CHANGE ANYTHING UNDER HERE!   //
+  // unless you know what you are doing! //
+  //-------------------------------------//
+  
+// initialize sdk
+cue.CorsairPerformProtocolHandshake();
 
+const ledsMap = {};
 
-//-------------------------------------//
-//    CONFIG - YOU CAN CHANGE THESE    //
-//-------------------------------------//
-var pubghealthx = 1456;
-var pubghealthy = 1383;
-//var mouse = robot.getMousePos();
-//console.log(mouse);
-
-//-------------------------------------//
-//  DONT CHANGE ANYTHING UNDER HERE!   //
-// unless you know what you are doing! //
-//-------------------------------------//
-
-console.log("Active!");
+const deviceCount = cue.CorsairGetDeviceCount();
+for (let di = 0; di < deviceCount; di++) {
+  const { type } = cue.CorsairGetDeviceInfo(di);
+  if (type == cue.CorsairDeviceType.CDT_Mouse || type == cue.CorsairDeviceType.CDT_Keyboard || type == cue.CorsairDeviceType.CDT_Headset) {
+    const deviceLeds = cue.CorsairGetLedPositionsByDeviceIndex(di);
+    ledsMap[di] = deviceLeds.map(dl => ({ ledId: dl.ledId, r: 0, g: 0, b: 0 }));
+  }
+}
 
 setInterval(() => {
-	// Get pixel color in hex format.
-	var Rgb = hexRgb(robot.getPixelColor(pubghealthx, pubghealthy));
-	var RgbSplit = Rgb.toString().split(',');
-	
-	if(RgbSplit[0] < 240){ var RgbSplit0 = (parseInt(RgbSplit[0], 10) + 14); }
-	if(RgbSplit[1] < 240){ var RgbSplit1 = (parseInt(RgbSplit[1], 10) + 14); }
-	if(RgbSplit[2] < 240){ var RgbSplit2 = (parseInt(RgbSplit[2], 10) + 14); }
-	
-	//console.log(parseInt(RgbSplit[1], 10) + 1);
-	
-	//console.log("Current Colour:" + RgbSplit[0] + ", " + RgbSplit[1] + ", " + RgbSplit[2] + " (#" + robot.getPixelColor(pubghealthx, pubghealthy) + ")");
-	for (i = 1; i < 155; i++) {
-		if (i !== 7) { //F6 Key - Makes the script beep for some reason, so its manually declaired below.
-			//console.log("Trying: " + i + " (" + String.fromCharCode(i) + ")")
-			cue.set(i, RgbSplit0, RgbSplit1, RgbSplit2, function() { /*console.log("Success: " + i)*/ }, true);
-		}
-	}
-	cue.set("CLK_F6", RgbSplit0, RgbSplit1, RgbSplit2, function() { /* console.log("Success: " + i) */ }, false);
-	cue.set("CLI_Invalid", 255, 0, 255, function() { /* console.log("Success: " + i) */ }, false);
-	//cue.set("CLK_G1", 255, 0, 255, function() { /* console.log("Success: " + i) */ }, false);
+  // Get pixel color in hex format.
+  const hex = robot.getPixelColor(pubghealthx, pubghealthy);
+  const color = hexRgb(hex);
 
-}, 250)
+  if (color.red < 240) { color.red += 14; }
+  if (color.green < 240) { color.green += 14; }
+  if (color.blue < 240) { color.blue += 14; }
+
+  //console.log(`Current Colour: ${color.red}, ${color.green}, ${color.blue} (#${hex})`);
+
+  // update colors for all devices
+  Object.values(ledsMap).forEach(leds => leds.forEach(led => {
+    led.r = color.red;
+    led.g = color.green;
+    led.b = color.blue;
+  }));
+
+  // prepare colors frame
+  Object.entries(ledsMap).forEach(([index, leds]) => cue.CorsairSetLedsColorsBufferByDeviceIndex(+index, leds));
+  // apply color
+  cue.CorsairSetLedsColorsFlushBuffer();
+}, 250);
+
+console.log("Active!");
